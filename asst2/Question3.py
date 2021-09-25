897d1533d0df6cee643bb877d7106408bea7fcf7
hanshopkins <69611816+hanshopkins@users.noreply.github.com>
2021-09-25 11:04:04 -0400

    Removing unnecessary plot

--- asst2/Question3.py
+++ asst2/Question3.py
@@ -10,7 +10,10 @@ rescaledX = 4*X-3
 coefs = chebyshev.chebfit(rescaledX,Y,20) #4 points picked because I can't see a difference with more
 
 def mylog2 (a):
-    assert(a > 0)
+    if isinstance(a, np.ndarray): #just making sure I'm not taking the log of a negative
+        assert(a.all() > 0)
+    else:
+        assert(a > 0)
     mantissa, exponent = np.frexp(a)
     return exponent + chebyshev.chebval(4*mantissa-3,coefs) #rescaling then throwing it in my approximation
 
@@ -18,3 +21,14 @@ def mylog2 (a):
 print(mylog2(8))
 print(mylog2(6.5))
 print(mylog2(2))
+print(mylog2(0.5))
+
+#and the last step is to convert the basis back to e
+def natural_log (a):
+    return mylog2(a)/mylog2(2.71828182845904523536) #should be enough
+
+#more testing
+print(natural_log(np.e))
+print(natural_log(11.56))
+print(natural_log(0.01))
+print(natural_log(np.asarray([np.e,11.56,0.01])))
