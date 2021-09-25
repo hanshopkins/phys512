be32b5683639a1ab4e4750f6c30485988494014a
hanshopkins <69611816+hanshopkins@users.noreply.github.com>
2021-09-25 11:31:26 -0400

    added bonus

--- asst2/Question3.py
+++ asst2/Question3.py
@@ -32,3 +32,27 @@ print(natural_log(np.e))
 print(natural_log(11.56))
 print(natural_log(0.01))
 print(natural_log(np.asarray([np.e,11.56,0.01])))
+
+###########################################################Bonus
+
+from numpy.polynomial import legendre
+
+legendre_coefs = legendre.legfit(rescaledX, Y, 20)
+
+def mylog2_legendre(a):
+    mantissa, exponent = np.frexp(a)
+    return exponent + legendre.legval(4*mantissa-3,legendre_coefs) #rescaling then throwing it in my approximation
+
+#computing the RMS
+testXValues = np.linspace(0.001,100, 300)
+realYValues = np.log2(testXValues)
+testYValues_cheby = mylog2(testXValues)
+testYValues_leg = mylog2_legendre(testXValues)
+
+def RMS (test, real):
+    assert (test.shape[0] == real.shape[0])
+    N = test.shape[0]
+    return np.sqrt(1/N*np.sum((real-test)**2))
+
+print("RMS of cheby is: ", RMS(testYValues_cheby, realYValues))
+print("RMS of legendre is: ", RMS(testYValues_leg, realYValues))
