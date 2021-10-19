9c1a61d902a450abf688b6bdc9ed4123bbcd86ff
hanshopkins <69611816+hanshopkins@users.noreply.github.com>
2021-10-19 05:44:33 -0400

    uploaded assignment 4

---
+++ asst4/Q3_analysis.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    chain = np.loadtxt(r"planck_chain.txt")
+    
+    parameter_output = np.empty([6,2])
+    for i in range(1,7):
+        parameter_output[i-1][0] = np.average(chain[:,i]) #averaging to find the parameter estimate
+        parameter_output[i-1][1] = np.std(chain[:,i]) #taking the standard deviation to find the error estimate
+    
+    print(parameter_output)
+    
+    plt.plot(chain[:,3])
+    plt.xlabel("nth step")
+    plt.ylabel(r"$\tau$")
+    plt.title("Checking convergence of chain")
+    plt.savefig(r"Q3mcplot.pdf")
