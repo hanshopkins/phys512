8c5b6cbb4d8e96ace3f1a6e6b4bf08881b8901a9
hanshopkins <69611816+hanshopkins@users.noreply.github.com>
2021-12-17 12:07:49 -0500

    Finished upload and added extras

--- Project/nbody.py
+++ Project/nbody.py
@@ -49,7 +49,46 @@ def get_force_from_pot (pos, pot, dims, res):
         forces[i][1] = (pot[(yidx-1)%res[1]][xidx] - pot[(yidx+1)%res[1]][xidx])/(2*dy)
     return forces 
 
-def nbodysim(particlesA, particlesB, sidelength, resolution, niter, plotFramesSkip, G_A, G_AB, G_B, R0, title, outputFileName, periodic, plotForces_ = False):
+##Functions for RK4
+def get_derivs(xx,nnA,nnB, kernelfft,sidelength,resolution,mA,mB, G_A, G_AB, G_B):
+    xA=xx[:nnA,:]%sidelength #modding side length for periodic case
+    vA=xx[nnA:2*nnA,:]
+    xB = xx[2*nnA:2*nnA+nnB,:]%sidelength
+    vB=xx[-nnB:,:]
+    
+    #getting forces
+    rhoA = get_rho(xA, mA, [[0,sidelength],[0,sidelength]], [resolution,resolution])
+    potA = np.fft.irfft2(np.fft.rfft2(rhoA)*kernelfft, [resolution,resolution])
+    rhoB = get_rho(xB, mB, [[0,sidelength],[0,sidelength]], [resolution,resolution])
+    potB = np.fft.irfft2(np.fft.rfft2(rhoB)*kernelfft, [resolution,resolution])
+    
+    # the forces differ by constants of G
+    fA = get_force_from_pot (xA, G_A * potA + G_AB * potB, [[0,sidelength],[0,sidelength]], [resolution,resolution])
+    fB = get_force_from_pot (xB, G_AB * potA + G_B * potB, [[0,sidelength],[0,sidelength]], [resolution,resolution])
+    
+    return np.vstack([vA,fA,vB,fB])
+
+
+def take_step_rk4_periodic(xA,vA,xB,vB,dt,kernelfft,sidelength,resolution,mA,mB, G_A, G_AB, G_B): #modified rk4
+    nnA=xA.shape[0]
+    nnB=xB.shape[0]
+    xx=np.vstack([xA,vA,xB,vB]) #we need to deal with the particles separately
+    k1=get_derivs(xx,nnA,nnB, kernelfft, sidelength,resolution,mA,mB, G_A, G_AB, G_B)
+    k2=get_derivs(xx+k1*dt/2,nnA,nnB, kernelfft, sidelength,resolution,mA,mB, G_A, G_AB, G_B)
+    k3=get_derivs(xx+k2*dt/2,nnA,nnB, kernelfft, sidelength,resolution,mA,mB, G_A, G_AB, G_B)
+    k4=get_derivs(xx+k3*dt,nnA,nnB, kernelfft, sidelength,resolution,mA,mB, G_A, G_AB, G_B)
+    
+    tot=(k1+2*k2+2*k3+k4)/6
+    
+    xA = (xA+tot[:nnA,:]*dt)%sidelength #modding side length because periodic
+    xB = (xB+tot[2*nnA:2*nnA+nnB,:]*dt)%sidelength
+    vA=vA+tot[nnA:2*nnA,:]*dt
+    vB=vB+tot[-nnB:,:]*dt
+    return xA,vA,xB,vB
+##################################### end of functions for RK4
+
+
+def nbodysim(particlesA, particlesB, sidelength, resolution, niter, plotFramesSkip, G_A, G_AB, G_B, R0, title, outputFileName, periodic, steptype = "leapfrog", plotForces_ = False):
     #checking whether it's periodic and setting the resolution appropriately
     if not periodic:
         resolution = resolution * 2
@@ -58,7 +97,7 @@ def nbodysim(particlesA, particlesB, sidelength, resolution, niter, plotFramesSk
         particlesA.positions += sidelength/4
         particlesB.positions += sidelength/4
     
-    dt = 0.1
+    dt = 0.05
     the_kernel = get_kernel(resolution, R0, 1.0)
     kernelfft = np.fft.rfft2(the_kernel)
     
@@ -67,43 +106,57 @@ def nbodysim(particlesA, particlesB, sidelength, resolution, niter, plotFramesSk
     
     t1 = time.time()
     for i in range(niter):
-        #calculating new positions
-        if periodic:
-            particlesA.positions = (particlesA.positions + particlesA.velocities * dt)%sidelength #the modulus is for periodic boundary conditions
-            particlesB.positions = (particlesB.positions + particlesB.velocities * dt)%sidelength
-        else:
-            particlesA.positions = (particlesA.positions + particlesA.velocities * dt)
-            particlesB.positions = (particlesB.positions + particlesB.velocities * dt)
-            #finding which particles are not in the region
-            if len(particlesA.positions) > 0:
-                indexes = np.asarray((np.min(particlesA.positions, axis = 1) >= sidelength/4) * (np.max(particlesA.positions, axis = 1) < 0.75*sidelength), dtype = "bool") #there's like 25% chance this works properly
-                particlesA.positions = particlesA.positions[indexes]
-                particlesA.velocities = particlesA.velocities[indexes]
-                particlesA.masses = particlesA.masses[indexes]
-            #repeating for particles B
-            if len(particlesB.positions) > 0:
-                #print((np.min(particlesA.positions, axis = 1) >= sidelength/4), (np.max(particlesA.positions, axis = 1) < 0.75*sidelength))
-                indexes = np.asarray((np.min(particlesB.positions, axis = 1) >= sidelength/4) * (np.max(particlesB.positions, axis = 1) < 0.75*sidelength), dtype = "bool")
-                particlesB.positions = particlesB.positions[indexes]
-                particlesB.velocities = particlesB.velocities[indexes]
-                particlesB.masses = particlesB.masses[indexes]
-            #I also have to remember to subtract its kinetic energy when I do that
+        if steptype == "leapfrog":
+            #calculating new positions
+            if periodic:
+                particlesA.positions = (particlesA.positions + particlesA.velocities * dt)%sidelength #the modulus is for periodic boundary conditions
+                particlesB.positions = (particlesB.positions + particlesB.velocities * dt)%sidelength
+            else:
+                particlesA.positions = (particlesA.positions + particlesA.velocities * dt)
+                particlesB.positions = (particlesB.positions + particlesB.velocities * dt)
+                #finding which particles are not in the region
+                if len(particlesA.positions) > 0:
+                    indexes = np.asarray((np.min(particlesA.positions, axis = 1) >= sidelength/4) * (np.max(particlesA.positions, axis = 1) < 0.75*sidelength), dtype = "bool") #there's like 25% chance this works properly
+                    particlesA.positions = particlesA.positions[indexes]
+                    particlesA.velocities = particlesA.velocities[indexes]
+                    particlesA.masses = particlesA.masses[indexes]
+                #repeating for particles B
+                if len(particlesB.positions) > 0:
+                    #print((np.min(particlesA.positions, axis = 1) >= sidelength/4), (np.max(particlesA.positions, axis = 1) < 0.75*sidelength))
+                    indexes = np.asarray((np.min(particlesB.positions, axis = 1) >= sidelength/4) * (np.max(particlesB.positions, axis = 1) < 0.75*sidelength), dtype = "bool")
+                    particlesB.positions = particlesB.positions[indexes]
+                    particlesB.velocities = particlesB.velocities[indexes]
+                    particlesB.masses = particlesB.masses[indexes]
+                
+            #calculating forces
+            rhoA = get_rho(particlesA.positions, particlesA.masses, [[0,sidelength],[0,sidelength]], [resolution,resolution])
+            potA = np.fft.irfft2(np.fft.rfft2(rhoA)*kernelfft, [resolution,resolution])
+            rhoB = get_rho(particlesB.positions, particlesB.masses, [[0,sidelength],[0,sidelength]], [resolution,resolution])
+            potB = np.fft.irfft2(np.fft.rfft2(rhoB)*kernelfft, [resolution,resolution])
             
-        #calculating forces
-        rhoA = get_rho(particlesA.positions, particlesA.masses, [[0,sidelength],[0,sidelength]], [resolution,resolution])
-        potA = np.fft.irfft2(np.fft.rfft2(rhoA)*kernelfft, [resolution,resolution])
-        rhoB = get_rho(particlesB.positions, particlesB.masses, [[0,sidelength],[0,sidelength]], [resolution,resolution])
-        potB = np.fft.irfft2(np.fft.rfft2(rhoB)*kernelfft, [resolution,resolution])
+            # the forces differ by constants of G
+            forcesA = get_force_from_pot (particlesA.positions, G_A * potA + G_AB * potB, [[0,sidelength],[0,sidelength]], [resolution,resolution])
+            forcesB = get_force_from_pot (particlesB.positions, G_AB * potA + G_B * potB, [[0,sidelength],[0,sidelength]], [resolution,resolution])
+            
+            #updating velocities
+            if len(particlesA.velocities > 0):
+                particlesA.velocities = particlesA.velocities + forcesA * (1/particlesA.masses)[:,None] * dt
+            if len(particlesB.velocities > 0):
+                particlesB.velocities = particlesB.velocities + forcesB * (1/particlesB.masses)[:,None] * dt
         
-        # the forces differ by constants of G
-        forcesA = get_force_from_pot (particlesA.positions, G_A * potA + G_AB * potB, [[0,sidelength],[0,sidelength]], [resolution,resolution])
-        forcesB = get_force_from_pot (particlesB.positions, G_AB * potA + G_B * potB, [[0,sidelength],[0,sidelength]], [resolution,resolution])
-        
-        #updating velocities
-        if len(particlesA.velocities > 0):
-            particlesA.velocities = particlesA.velocities + forcesA * (1/particlesA.masses)[:,None] * dt
-        if len(particlesB.velocities > 0):
-            particlesB.velocities = particlesB.velocities + forcesB * (1/particlesB.masses)[:,None] * dt
+        elif steptype == "rk4" or steptype == "RK4":
+            if periodic:
+                particlesA.positions, particlesA.velocities, particlesB.positions, particlesB.velocities = take_step_rk4_periodic(particlesA.positions, particlesA.velocities, particlesB.positions, particlesB.velocities,dt, kernelfft, sidelength,resolution, particlesA.masses, particlesB.masses, G_A, G_AB, G_B)
+                
+                ### I need to recalculate these again for the energy
+                rhoA = get_rho(particlesA.positions, particlesA.masses, [[0,sidelength],[0,sidelength]], [resolution,resolution])
+                potA = np.fft.irfft2(np.fft.rfft2(rhoA)*kernelfft, [resolution,resolution])
+                rhoB = get_rho(particlesB.positions, particlesB.masses, [[0,sidelength],[0,sidelength]], [resolution,resolution])
+                potB = np.fft.irfft2(np.fft.rfft2(rhoB)*kernelfft, [resolution,resolution])
+                #
+            else:
+                print("do non periodic") #note I couldn't figure out a good way to implement this one
+        else: print("unknown step type")
         
         if i % plotFramesSkip == 0:
             ##calculating energy####################
@@ -155,20 +208,21 @@ def nbodysim(particlesA, particlesB, sidelength, resolution, niter, plotFramesSk
     print("Time it took:", time.time()-t1)
 
 if __name__ == "__main__":
-    particlesA, particlesB, sidelength = ic.mixOfBoth(100000, 0, "good seed", 1000) #this is where you choose initial conditions
+    particlesA, particlesB, sidelength = ic.mixOfBoth(100, 900, "phys512", 1) #this is where you choose initial conditions
     
     resolution = 256
-    niter = 400
+    niter = 500
     plotFramesSkip = 1
     plotForces = False
-    periodic = False
+    periodic = True
+    steptype = "leapfrog" #either leapfrog or RK4
     
-    G_A = 0.1 #force scaling factor. THere's different ones for the different possible interactions
-    G_AB = 0.01
-    G_B = -0.01
+    G_A = 0.005 #force scaling factor. THere's different ones for the different possible interactions
+    G_AB = 0.005
+    G_B = -0.02
     R0 = 0.01
     
-    title = "Many Particles"
-    outputFileName = "leapfrog_nonperiodic"
+    title = "A and B Particles"
+    outputFileName = "Extra_A_and_B"
     
-    nbodysim(particlesA, particlesB, sidelength, resolution, niter, plotFramesSkip, G_A, G_AB, G_B, R0, title, outputFileName, periodic, plotForces_ = plotForces)
+    nbodysim(particlesA, particlesB, sidelength, resolution, niter, plotFramesSkip, G_A, G_AB, G_B, R0, title, outputFileName, periodic, steptype, plotForces_ = plotForces)
